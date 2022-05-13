8feeef8945f5b7fe04966e049888b8f54ba6b52e
Pierre Duchateau <pad@dabla.com>
2022-05-13 19:07:11 +0200

    add storybook

---
+++ src/application/routes/demandeListPage/DemandeListPage.stories.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { DemandeListPage } from './DemandeListPage';
+
+export default { title: 'Liste de demandes', component: DemandeListPage };
+
+export const Vide = () => <DemandeListPage demandes={[]} />;
+
+export const NonVide = () => (
+  <DemandeListPage
+    demandes={[
+      {
+        id: '123',
+        type: 'réclamation',
+        déposéeLe: new Date().getTime(),
+      },
+    ]}
+  />
+);
