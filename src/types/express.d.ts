7b49494f0a442551f4421971af20fc78c6ec1b00
Pierre Duchateau <pad@dabla.com>
2022-06-14 10:12:20 +0200

    express.d.ts does not import OklmUser

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,7 @@
-import { User as OklmUser } from '../modules/authZ';
-
 declare module Express {
   // eslint-disable-next-line
-  interface Request {
-    user: OklmUser;
+  export interface Request {
+    user: { id: string; role: string };
     kauth: any;
   }
 }
