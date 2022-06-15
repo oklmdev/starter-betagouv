bc4a0447c31c21ddb662957c70f6f01805dcffa7
Pierre Duchateau <pad@dabla.com>
2022-06-15 11:39:58 +0200

    Mise à jour de storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/application/routes/demandeListPage/DemandeListPage.stories.tsx'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
 };
