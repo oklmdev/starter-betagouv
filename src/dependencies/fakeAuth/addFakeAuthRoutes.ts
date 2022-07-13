e42dc53b63b5ce0830b942c663371584aa895736
Pierre-A. Duchateau <pad@dabla.com>
2022-07-13 15:29:28 +0200

    Retirer express-async-handler

--- src/dependencies/fakeAuth/addFakeAuthRoutes.ts
+++ src/dependencies/fakeAuth/addFakeAuthRoutes.ts
@@ -1,38 +1,31 @@
 import { Express } from 'express';
-import asyncHandler from '../../libs/asyncHandler';
 import { responseAsHtml } from '../../libs/responseAsHtml';
 import { FakeConnexionPage } from './fakeConnexionPage';
 import { getFakeUsers } from './getFauxUtilisateurs.query';
 
 export const addFakeAuthRoutes = (app: Express) => {
-  app.get(
-    '/login.html',
-    asyncHandler(async (request, response) => {
-      const fakeUsers = await getFakeUsers();
-      const { redirectTo } = request.query;
+  app.get('/login.html', async (request, response) => {
+    const fakeUsers = await getFakeUsers();
+    const { redirectTo } = request.query;
 
-      responseAsHtml(
-        request,
-        response,
-        FakeConnexionPage({ fakeUsers, redirectTo: typeof redirectTo === 'string' ? redirectTo : undefined })
-      );
-    })
-  );
+    responseAsHtml(
+      request,
+      response,
+      FakeConnexionPage({ fakeUsers, redirectTo: typeof redirectTo === 'string' ? redirectTo : undefined })
+    );
+  });
 
-  app.post(
-    '/login.html',
-    asyncHandler(async (request, response) => {
-      const fakeUsers = await getFakeUsers();
-      const { userId, redirectTo } = request.body;
+  app.post('/login.html', async (request, response) => {
+    const fakeUsers = await getFakeUsers();
+    const { userId, redirectTo } = request.body;
 
-      const user = fakeUsers.find((user) => user.userId === userId);
+    const user = fakeUsers.find((user) => user.userId === userId);
 
-      if (!user) {
-        return response.status(403);
-      }
+    if (!user) {
+      return response.status(403);
+    }
 
-      request.session.user = { id: userId, role: user.role };
-      response.redirect(redirectTo || '/');
-    })
-  );
+    request.session.user = { id: userId, role: user.role };
+    response.redirect(redirectTo || '/');
+  });
 };
