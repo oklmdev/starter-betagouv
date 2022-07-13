794349ff147d132eb97c704ee017f1bf7b2d7ddd
Pierre-A. Duchateau <pad@dabla.com>
2022-07-13 16:12:22 +0200

    Retirer table utilisateur_keycloak

--- src/tables/index.ts
+++ src/tables/index.ts
@@ -1,3 +1,2 @@
 export * from './demandes';
 export * from './tables';
-export * from './utilisateur_keycloak';
--- src/tables/utilisateur_keycloak/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './onUtilisateurInscritViaIdentityProvider';
-export * from './utilisateur_keycloak';
