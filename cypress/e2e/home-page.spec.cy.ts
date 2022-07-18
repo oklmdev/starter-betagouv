75a5ddc8f2b0e54ccbba3b10cdf84129e6a745ec
Marc Gavanier <marc.gavanier@gmail.com>
2022-07-18 18:41:15 +0200

    fix: code style

--- cypress/e2e/home-page.spec.cy.ts
+++ cypress/e2e/home-page.spec.cy.ts
@@ -1,17 +1,17 @@
 describe('Home page', (): void => {
   it('should display application name', (): void => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/');
 
-    cy.contains('La célérité c\'est bien mais avec modération')
+    cy.contains("La célérité c'est bien mais avec modération");
   });
 
   it('should navigate to login page', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/');
 
     cy.contains('Connexion').click();
 
     cy.url().should('include', '/login.html');
 
-    cy.contains('Login de demo (vous serez redirigés vers )')
-  })
-})
+    cy.contains('Login de demo (vous serez redirigés vers )');
+  });
+});
