ed7a55959948bdc4f27e4a0b7e03fcd49716342a
Pierre Duchateau <pad@dabla.com>
2022-09-05 19:18:43 +0200

    eslint for vscode

--- .tooling/.eslint/.eslintrc.cjs
+++ .tooling/.eslint/.eslintrc.cjs
@@ -1,38 +1,36 @@
 // Lint the files included in each typescript project with common rules
-const projects = [
-  "./../../tsconfig.json",
-];
+const projects = ['./../../tsconfig.json'];
 // Lint project using its tsconfig.json.
 const lintProjects = () => {
   return [
     {
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
         //'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        "prettier",
+        'prettier'
       ],
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         tsconfigRootDir: __dirname,
         project: projects,
         ecmaVersion: 2020,
-        sourceType: "module"
+        sourceType: 'module'
       },
-      plugins: ["@typescript-eslint"],
+      plugins: ['@typescript-eslint'],
       rules: {
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/prefer-as-const": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/prefer-namespace-keyword": "off",
-        //...require("./eslint.rules.cjs"),
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/prefer-as-const': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/prefer-namespace-keyword': 'off'
+        //...require('./eslint.rules.cjs')
         //...require("./typescript-eslint.rules.cjs"),
-      },
-    },
+      }
+    }
   ];
 };
 
@@ -41,13 +39,12 @@ const lintProjectsWithoutTests = () => {
   return [...lintProjects()];
 };
 
-
 module.exports = {
   root: true,
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
-  overrides: [...lintProjectsWithoutTests()],
+  overrides: [...lintProjectsWithoutTests()]
 };
