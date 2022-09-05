8536bb480cd1daf303bc47eeb3d11cdfb1c0a03b
Romain Cambonie <romain.cambonie@gmail.com>
2022-09-05 18:11:12 +0200

    build(commands): fixed configuration for lint prettier and test

---
+++ .tooling/.eslint/typescript-eslint-test.rules.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-assertions': [
+        "off",
+        {
+            assertionStyle: "as",
+            objectLiteralTypeAssertions: "never",
+        },
+    ],
+}
---
+++ .tooling/.prettier/.prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  "printWidth": 128,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": true,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "bracketSameLine": true,
+  "arrowParens": "always",
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "ignore",
+  "vueIndentScriptAndStyle": false,
+  "endOfLine": "lf",
+  "embeddedLanguageFormatting": "auto"
+}
