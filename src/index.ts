708ca7071754c6791543923792b0aaf2d26e9273
Pierre Duchateau <pad@dabla.com>
2022-09-23 15:04:12 +0200

    Simplification: retirer couche persistence

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,8 @@ import express, { Express } from 'express';
 require('express-async-errors');
 import session from 'express-session';
 import path from 'node:path';
-import { tables } from './tables';
 import { sessionStore } from './dependencies/session';
 import { pageRouter } from './pages';
-import { actionsRouter } from './actions';
-import { registerAuth } from './dependencies/authn';
-import { subscribeAll } from './dependencies/eventStore';
 
 const PORT: number = parseInt(process.env.PORT ?? '3000');
 
@@ -35,20 +31,11 @@ app.use(
   })
 );
 
-registerAuth(app);
-
 app.use(pageRouter);
-app.use(actionsRouter);
 
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.listen(PORT, (): void => {
   // eslint-disable-next-line no-console
   console.log('Server listening to port', PORT);
-
-  subscribeAll(async (event) => {
-    for (const projectionTable of tables) {
-      await projectionTable.handleEvent(event);
-    }
-  });
 });
